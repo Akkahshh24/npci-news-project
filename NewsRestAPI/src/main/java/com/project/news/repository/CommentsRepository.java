421f37bbf8a2ac9421e5a7aea121e5a5b4d9f8ae
Akkahshh24 <104076460+Akkahshh24@users.noreply.github.com>
2022-08-05 20:41:41 +0530

    Updated likes and comments repository

--- NewsRestAPI/src/main/java/com/project/news/repository/CommentsRepository.java
+++ NewsRestAPI/src/main/java/com/project/news/repository/CommentsRepository.java
@@ -1,5 +1,7 @@
 package com.project.news.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,5 +11,11 @@ import com.project.news.entity.Comments;
 @RepositoryRestResource(path="comments")
 @CrossOrigin("http://localhost:4200/")
 public interface CommentsRepository extends JpaRepository<Comments, Integer>{
+	
+	public List<Comments> findByUserId(Integer userId);
+	
+	public List<Comments> findByArticleId(Integer articleId);
+	
+	public Integer countByArticleId(Integer articleId);
 
 }
--- NewsRestAPI/src/main/java/com/project/news/repository/LikesRepository.java
+++ NewsRestAPI/src/main/java/com/project/news/repository/LikesRepository.java
@@ -1,5 +1,7 @@
 package com.project.news.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,5 +11,9 @@ import com.project.news.entity.Likes;
 @RepositoryRestResource(path="likes")
 @CrossOrigin("http://localhost:4200/")
 public interface LikesRepository extends JpaRepository<Likes, Integer> {
+	
+	public List<Likes> findByUserId(Integer userId); 
+	
+	public Integer countByArticleId(Integer articleId);
 
 }
